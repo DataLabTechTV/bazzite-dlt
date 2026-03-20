539a56fe98e84b4876eca640dabdf1d98ea585d9
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-03-20 19:27:26 +0000

    fix: ImageMagick dependencies for iOS HEIC, simplify overall access to non-enabled repos

--- build_files/base.sh
+++ build_files/base.sh
@@ -10,6 +10,4 @@ dnf5 -y copr enable avengemedia/dms
 dnf5 -y install niri
 dnf5 -y copr disable avengemedia/dms
 
-dnf5 config-manager setopt terra.enabled=1
-dnf5 -y install noctalia-shell
-dnf5 config-manager setopt terra.enabled=0
+dnf5 -y --enablerepo=terra install noctalia-shell
--- build_files/graphics.sh
+++ build_files/graphics.sh
@@ -2,13 +2,8 @@
 
 set -euxo pipefail
 
-dnf5 -y install \
-	"https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
-	"https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-
-dnf5 -y install ImageMagick ImageMagick-heic libheif-freeworld libde265
-
-dnf config-manager --disable rpmfusion-free rpmfusion-nonfree
+dnf5 -y install ImageMagick-heic
+dnf5 -y --enablerepo=rpmfusion-nonfree install libheif-freeworld
 
 export UV_CACHE_DIR=/var/cache/uv
 uv tool install --link-mode=copy rembg[gpu,cli]
