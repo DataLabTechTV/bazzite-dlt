a0cecbdaee6282df2d8ee25cf86d6d6b3f51d5d9
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-03-21 17:51:27 +0000

    fix: replace duckdb install script by binary download, so we can drop to custom location

--- build_files/data.sh
+++ build_files/data.sh
@@ -8,7 +8,8 @@ source "$(dirname "${BASH_SOURCE[0]}")/uv-env.sh"
 dnf5 -y install sqlite3
 dnf5 -y install miller gnuplot
 
-curl https://install.duckdb.org | sh
+curl -L https://install.duckdb.org/v1.5.0/duckdb_cli-linux-amd64.zip | funzip >/var/usr/local/bin/duckdb
+chmod +x /var/usr/local/bin/duckdb
 
 uv tool install --link-mode=copy termgraph
 uv tool install --link-mode=copy visidata
