d0af3e0554aaa4d6adf5005349f43f62e3841b70
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-03-21 18:40:38 +0000

    fix: should be docker-compose-switch

--- build_files/containers.sh
+++ build_files/containers.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 # shellcheck source=go-env.sh
 source "$(dirname "${BASH_SOURCE[0]}")/go-env.sh"
 
-dnf5 -y install docker-cli docker-compose
+dnf5 -y install docker-cli docker-compose-switch
 
 go install github.com/jesseduffield/lazydocker@latest
 go install github.com/sigstore/cosign/v3/cmd/cosign@latest
