235cc0d2544fbda4745f43db10037d8c9619d9c1
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-03-22 22:00:00 +0000

    fix: bootc linter complained about opt and the files weren't there anyway

--- build_files/uv-env.sh
+++ build_files/uv-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export UV_CACHE_DIR=/var/cache/uv
-export UV_TOOL_DIR=/var/opt
-export UV_TOOL_BIN_DIR=/usr/bin
+export UV_TOOL_DIR=/var/usrlocal
+export UV_TOOL_BIN_DIR=/var/usrlocal/bin
