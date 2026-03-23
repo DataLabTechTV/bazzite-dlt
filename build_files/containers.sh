cf1bcd1f5aa070b9a172408ddde97c3d47531b81
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-03-23 18:25:30 +0000

    fix: no longer using /usr/local, as it shouldn't be touched

--- build_files/containers.sh
+++ build_files/containers.sh
@@ -6,6 +6,8 @@ set -euxo pipefail
 source "$(dirname "${BASH_SOURCE[0]}")/go-env.sh"
 
 dnf5 -y install docker-cli docker-compose-switch
+mv /usr/bin/docker /usr/bin/docker.real
+mv /usr/bin/docker-compose /usr/bin/docker-compose.real
 
 go install github.com/jesseduffield/lazydocker@latest
 go install github.com/sigstore/cosign/v3/cmd/cosign@latest
