fbb636e058de8c992c08079f4106cfc5a3c6c870
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-04-04 13:34:03 +0100

    fix(kernel): dracut command similar to official bazzite

--- build_files/90-kernel.sh
+++ build_files/90-kernel.sh
@@ -3,4 +3,7 @@
 set -euxo pipefail
 
 kver=$(cd /usr/lib/modules && echo *)
-dracut -vf "/usr/lib/modules/$kver/initramfs.img" "$kver"
+dracut --no-hostonly --kver "$kver" --reproducible --zstd -v --add ostree --add fido2 -f \
+	"/usr/lib/modules/$kver/initramfs.img"
+
+chmod 0600 "/usr/lib/modules/$kver/initramfs.img"
